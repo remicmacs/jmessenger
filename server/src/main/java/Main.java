433b9d0a9d121eb2b4d1ad51c610f185f66bfbfa
remicmacs <rembourgeon@gmail.com>
2019-02-05 19:46:52 +0100

    Add server subtree with Maven
    Maven dependency management because I would never accept to work in any
    other language without dependency management tooling.
    
    MariaDB added to dependencies and tested.
    
    Phat description.
    
    - Thingy #1
    - Thingy #2
    
    Why:
    
    - Because this
    - And that

---
+++ server/src/main/java/Main.java
@@ -0,0 +1,19 @@
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Hello, world");
+        try (Connection conn = DriverManager.getConnection("jdbc:mariadb" +
+                "://localhost/", "root", "root")) {
+            // create a Statement
+            try (Statement stmt = conn.createStatement()) {
+                //execute query
+                try (ResultSet rs = stmt.executeQuery("SELECT 'Hello World!'")) {
+                    //position result to first
+                    rs.first();
+                    System.out.println(rs.getString(1)); //result is "Hello World!"
+                }
+            }
+        }
+    }
+}
