5ed882aab16977087838170c1bcb2795c00a82d6
remicmacs <rembourgeon@gmail.com>
2019-03-03 22:03:04 +0100

    Handle basic interruption signals
    C^C and C^D are handled in the main loop of the program. It is an
    improvement because when an interrupt signal is received by the program,
    it should be able to save what is necessary as quickly as possible.

--- jmgr/src/main/java/us/hourgeon/jmessenger/server/Main.java
+++ jmgr/src/main/java/us/hourgeon/jmessenger/server/Main.java
@@ -3,19 +3,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
-import java.net.UnknownHostException;
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.channels.SelectionKey;
-import java.nio.channels.Selector;
-import java.nio.channels.ServerSocketChannel;
-import java.nio.channels.SocketChannel;
-import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.sql.*;
-import java.util.Iterator;
-import java.util.Set;
-
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
@@ -23,7 +10,7 @@ import org.java_websocket.server.WebSocketServer;
 public class Main {
     static class ChatServer extends WebSocketServer {
 
-        public ChatServer( int port ) throws UnknownHostException {
+        public ChatServer( int port ) {
             super( new InetSocketAddress( port ) );
         }
 
@@ -31,18 +18,23 @@ public class Main {
             super( address );
         }
 
-
         @Override
         public void onOpen(WebSocket conn, ClientHandshake handshake) {
-            conn.send("Welcome to the server!"); //This method sends a message to the new client
-            broadcast( "new connection: " + handshake.getResourceDescriptor() ); //This method sends a message to all clients connected
-            System.out.println( conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!" );
+            //This method sends a message to the new client only
+            conn.send("Welcome to the server!");
+            //This method sends a message to all clients connected
+            broadcast( "new connection: " + handshake.getResourceDescriptor() );
+            System.out.println(
+                    conn.getRemoteSocketAddress()
+                            .getAddress()
+                            .getHostAddress()
+                    + " entered the room!"
+            );
 
         }
 
         @Override
         public void onClose(WebSocket conn, int code, String reason, boolean remote) {
-
             broadcast( conn + " has left the room!" );
             System.out.println( conn + " has left the room!" );
 
@@ -50,6 +42,7 @@ public class Main {
 
         @Override
         public void onMessage(WebSocket conn, String message) {
+            // Include thread instantiation for message handling here
             broadcast( message );
             System.out.println( conn + ": " + message );
 
@@ -70,71 +63,11 @@ public class Main {
             setConnectionLostTimeout(100);
         }
     };
-    public static void main(String[] args) throws SQLException, IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
+
         System.out.println("Server booting...");
 
-        /* Server with Sockets and Selector of channels
-        // Create the server socket channel
-        ServerSocketChannel server = ServerSocketChannel.open();
-        // nonblocking I/O
-        server.configureBlocking(false);
-
-        server.socket().bind(new InetSocketAddress(8000));
-        System.out.println("Server is active at port 8000");
-
-        Selector selector = Selector.open();
-        server.register(selector, SelectionKey.OP_ACCEPT);
-
-        while (selector.select() > 0) {
-
-            Set<SelectionKey> keys = selector.selectedKeys();
-            Iterator<SelectionKey> i = keys.iterator();
-
-            System.out.println("[ " + keys.size() + " ]");
-
-            while (i.hasNext()) {
-                SelectionKey key = i.next();
-                // Remove the current key
-                i.remove();
-
-                // a client required a connection
-                if (key.isAcceptable()) {
-                    // get client socket channel
-                    SocketChannel client = server.accept();
-                    // Non Blocking I/O
-                    client.configureBlocking(false);
-                    // recording to the selector (reading)
-                    client.register(selector, SelectionKey.OP_READ);
-                    continue;
-                }
-
-                // the server is ready to read
-                if (key.isReadable()) {
-
-                    SocketChannel client = (SocketChannel) key.channel();
-
-                    // Read byte coming from the client
-                    int BUFFER_SIZE = 1024;
-                    ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
-
-
-                    if (client.read(buffer) != -1 ) {
-
-                        // Show bytes on the console
-                        buffer.flip();
-                        Charset charset = Charset.forName("UTF-8");
-                        CharsetDecoder decoder = charset.newDecoder();
-                        CharBuffer charBuffer = decoder.decode(buffer);
-                        System.out.println("[" + charBuffer.toString() + "]");
-                    } else {
-                        // IF EOF, close the channel
-                        client.close();
-                    }
-                }
-            }
-        }*/
-
-        int port = 8887; // 843 flash policy port
+        int port = 38887;
         try {
             port = Integer.parseInt( args[ 0 ] );
         } catch ( Exception ex ) {
@@ -143,19 +76,38 @@ public class Main {
         s.start();
         System.out.println( "ChatServer started on port: " + s.getPort() );
 
-        BufferedReader sysin = new BufferedReader( new InputStreamReader( System.in ) );
-        while ( true ) {
-            String in = sysin.readLine();
-            s.broadcast( in );
-            if( in.equals( "exit" ) ) {
+        // Shutdown hook to do some last-minute cleaning if interrupt signal
+        // is received.
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.err.println("Shutdown signal received\n" +
+                    "Saving system state...");
+            s.broadcast("Server shutting down...");
+
+            try {
                 s.stop(1000);
-                break;
+            } catch (InterruptedException e) {
+                System.err.println("Wow server shutdown got interrupted, bad " +
+                        "luck.");
+                e.printStackTrace();
+            }
+        }));
+
+        BufferedReader sysin = new BufferedReader(
+                new InputStreamReader( System.in )
+        );
+        while ( true ) {
+            try {
+                String in = sysin.readLine();
+                if( in.equals( "exit" ) ) {
+                    System.exit(0);
+                }
+                s.broadcast( in );
+            } catch (NullPointerException nullPEx) {
+                System.err.println("System side of stream closed");
+                /* Call to System.exit() will invoke shutdown hook registered
+                    above. */
+                System.exit(1);
             }
         }
-
-
-        System.out.println("Server stopping...");
-
-
     }
 }
